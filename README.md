62d55b1efca18c837d0c173473dbd502d7a30b43
beepa <admin@beartigerproductions.com>
2021-08-04 13:06:07 +0200

    upgrade changes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# paynterperformance
+# Kai Paytner Advisory
+
+**We are currently deploying changes to the website**
+
+**Please check in after a few minutes**
+
+If an urgent query needs to be made, please contact us at info@kaipaynter.com
+
+Thank you
--- gatsby-config.js
+++ gatsby-config.js
@@ -13,68 +13,18 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
-    // {
-    //   resolve: 'gatsby-plugin-sitemap',
-    //   options: {
-    //     options: {
-    //       query: `
-    //       {
-    //         allSitePage {
-    //           nodes {
-    //             path
-    //           }
-    //         }
-    //         allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
-    //           nodes {
-    //             ... on WpPost {
-    //               uri
-    //               modifiedGmt
-    //             }
-    //             ... on WpPage {
-    //               uri
-    //               modifiedGmt
-    //             }
-    //           }
-    //         }
-    //       }
-    //     `,
-    //       resolveSiteUrl: () => siteUrl,
-    //       resolvePages: ({
-    //         allSitePage: { nodes: allPages },
-    //         allWpContentNode: { nodes: allWpNodes },
-    //       }) => {
-    //         const wpNodeMap = allWpNodes.reduce((acc, node) => {
-    //           const { uri } = node
-    //           acc[uri] = node
-  
-    //           return acc
-    //         }, {})
-  
-    //         return allPages.map(page => {
-    //           return { ...page, ...wpNodeMap[page.path] }
-    //         })
-    //       },
-    //       serialize: ({ path, modifiedGmt }) => {
-    //         return {
-    //           url: path,
-    //           lastmod: modifiedGmt,
-    //         }
-    //       },
-    //     },
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: 'gatsby-starter-default',
-    //     short_name: 'starter',
-    //     start_url: '/',
-    //     background_color: '#663399',
-    //     theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //     icon: 'src/assets/images/logo2.png', // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/logo2.png', // This path is relative to the root of the site.
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
   ],
