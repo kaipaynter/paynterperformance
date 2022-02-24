ef7313da73c5b1b6157124bd044a478293e16248
beepa <admin@geartigerproductions.com>
2022-02-24 11:38:13 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3fd3a239176ff59018a9.js"
+    "url": "webpack-runtime-04c17cbd6f951ea09ffb.js"
   },
   {
     "url": "framework-3fcfa884e93dfe69e633.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e08572a98db17af8215c2bfa739b5d0e"
+    "revision": "17bb735648fea8a0a0995c58148d1c2c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-f0b1862c24593d464a07.js"
