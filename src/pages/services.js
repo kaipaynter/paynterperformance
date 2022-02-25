5ba80c11048de48e0a5899d10500fad527b5a59f
beepa <admin@geartigerproductions.com>
2022-02-25 10:16:32 +0200

    fixed images

--- src/pages/services.js
+++ src/pages/services.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { Waypoint } from 'react-waypoint'
 import Layout from '../components/layout'
 import archetype from '../assets/images/servicespage/archetype.jpg'
-import kapmanagement from '../assets/images/servicespage/KAP_Management_2.jpg'
+import kapmanagement from '../assets/images/servicespage/KAP_Management_3.jpg'
 import secrets from '../assets/images/servicespage/Secrets_1.jpg'
 import generalamericanaccent from '../assets/images/servicespage/Gen_Am_B_1.jpg'
 import yourcareer from '../assets/images/servicespage/career2.jpg'
 import ServicesNav from '../components/ServicesNav'
 import coaching from '../assets/images/servicespage/c1_cropped.png'
 import { Helmet } from 'react-helmet'
-import { StaticImage } from 'gatsby-plugin-image'
 
 //Icons
 import Al_a_Carte from '../assets/images/servicespage/Al_a_Carte_Icon.jpg'
@@ -292,38 +291,6 @@ class Services extends React.Component {
                   <img src={kapmanagement} alt="KAP Management" />
                 </span>
                 <p />
-                <ul className="features">
-                  <li>
-                    <span>
-                      <StaticImage
-                        src="../assets/images/servicespage/KPM_1.jpeg"
-                        alt="Coaching"
-                        placeholder="blurred"
-                        objectFit="contain"
-                      />
-                    </span>
-                  </li>
-                  <li>
-                    <span>
-                      <StaticImage
-                        src="../assets/images/servicespage/KPM_2.jpeg"
-                        alt="Coaching"
-                        placeholder="blurred"
-                        objectFit="contain"
-                      />
-                    </span>
-                  </li>
-                  <li>
-                    <span>
-                      <StaticImage
-                        src="../assets/images/servicespage/KPM_3.jpeg"
-                        alt="Coaching"
-                        placeholder="blurred"
-                        objectFit="contain"
-                      />
-                    </span>
-                  </li>
-                </ul>
                 <p className="centeredText">
                   <em>Click image for further details.</em>
                 </p>
